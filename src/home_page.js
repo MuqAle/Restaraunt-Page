181450bc513e33e4c7fa94098b837aa6c7766dfa
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-01-11 14:14:24 -0500

    create heading and home page skeleton

---
+++ src/home_page.js
@@ -0,0 +1,23 @@
+
+function mamaImage(){
+    const mamaImg = document.createElement('div');
+    mamaImg.classList.add('img_container');
+    const topImg = document.createElement('img');
+    topImg.classList.add('top_img');
+    const bottomImg = document.createElement('img');
+    bottomImg.classList.add('bottom_img');
+    mamaImg.appendChild(topImg);
+    mamaImg.appendChild(bottomImg);
+    return mamaImg
+}
+
+function aboutText(){
+    const textBox = document.createElement('div');
+    const welcomeText = document.createElement('p');
+    const aboutText = document.createElement('p');
+    welcomeText.textContent = 'Welcome to Cooking Mama';
+    aboutText.textContent = "There's room for improvement to even better than Mama";
+    textBox.appendChild(welcomeText);
+    textBox.appendChild(aboutText);
+    return textBox
+}
